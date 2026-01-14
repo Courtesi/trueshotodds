493d3c3caf5cac993add4141aac4527ff4730c71
Courtesi <c.chang673@gmail.com>
2026-01-14 13:55:02 -0800

    removal of hooks and scripts

--- scripts/metadata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for key in $(docker exec tso-redis redis-cli KEYS "sportsbook:*:metadata"); do
-	echo "Key: $key"
-	docker exec tso-redis redis-cli HGETALL "$key" | paste -d': ' - -
-	echo ""
-done
